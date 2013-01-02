b63e239ff2ead076cc3174a04e42d6a6d5291ac3
Alex Batko <alex@exitloop.ca>
2013-01-02 14:31:03 -0800

    added link to altpass on RubyGems; bumped version

--- lib/alexpass.rb
+++ lib/alexpass.rb
@@ -1,7 +1,7 @@
 class Alexpass
 
-  VERSION = '0.1.2'
-  DEPRECATED = 'Sorry, but this gem has been replaced by Altpass: https://github.com/abatko/altpass'
+  VERSION = '0.1.3'
+  DEPRECATED = "Sorry, but this gem ('alexpass') has been replaced by 'altpass'. See RubyGems https://rubygems.org/gems/altpass and GitHub https://github.com/abatko/altpass"
 
   def self.generate(options={})
     warn DEPRECATED
